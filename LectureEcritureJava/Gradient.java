44ee0937bec147f92c2680b8282b1555c40a89e2
NOAM LACOMBE <noam.lacombe@iut.local>
2026-09-08 15:04:21 +0200

    Ajoute Gradient et Image en java

---
+++ LectureEcritureJava/Gradient.java
@@ -0,0 +1,20 @@
+public class Gradient {
+    public static void main(String[] args) {
+        Image img = new Image(200, 100);
+
+        // Génération du dégradé de bleu
+        for (int y = 0; y < img.getWidth(); y++) {
+            for (int x = 0; x < img.getHeight(); x++) {
+                int bleu = 0; // quel calcule ?
+                img.setPixel(x, y, 0, 0, bleu);
+            }
+        }
+
+        try {
+            img.save_txt("gradient.ppm");
+            System.out.println("Dégradé créé avec succès !");
+        } catch (Exception e) {
+            System.err.println("Erreur lors de la création du dégradé : " + e.getMessage());
+        }
+    }
+}

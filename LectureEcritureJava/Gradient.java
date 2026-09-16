b2711fc85af816f32d7f549b651382ea81591fc3
Noaml <noaml@gmail.com>
2026-09-16 09:14:14 +0200

    correction write_bin

--- LectureEcritureJava/Gradient.java
+++ LectureEcritureJava/Gradient.java
@@ -1,25 +1,25 @@
 public class Gradient {
+
     public static void main(String[] args) {
         Image img = new Image(200, 100);
 
         // Génération du dégradé de bleu
-        for (int y = 0; y < img.getHeight(); y++) {
-            for (int x = 0; x < img.getWidth(); x++) {
-                int bleu = (int) (255.0 * x / (img.getWidth() - 1));
+        System.out.print("" + img.getHeight() + img.getWidth() + "");
+
+
+        for (int y = 0; y < img.getHeight() ; y++) {
+
+            for (int x = 0; x < img.getWidth() ; x++) {
+
+                int bleu = ((x*255)/200); // quel calcule ? = produit en croix ((200*100)/255 = 0.78)
                 img.setPixel(x, y, 0, 0, bleu);
             }
         }
 
         try {
             img.save_txt("gradient.ppm");
+            img.save_bin("gradientBin.ppm");
             System.out.println("Dégradé créé avec succès !");
-        } catch (Exception e) {
-            System.err.println("Erreur lors de la création du dégradé : " + e.getMessage());
-        }
-		
-		try {
-            img.write_bin("gradientBin.ppm");
-            System.out.println("Dégradé binaire créé avec succès !");
         } catch (Exception e) {
             System.err.println("Erreur lors de la création du dégradé : " + e.getMessage());
         }
--- LectureEcritureJava/Image.java
+++ LectureEcritureJava/Image.java
@@ -3,10 +3,10 @@ import java.io.IOException;
 import java.io.FileOutputStream;
 
 public class Image {
+
     private int width;
     private int height;
-    // pixels[y][x][0=R,1=G,2=B]
-    private int[][][] pixels; // pixels[y][x][0=R,1=G,2=B]
+    private int[][][] pixels; // pixels[x][y][0=R,1=G,2=B]
 
     public int getWidth() { return width; }
     public int getHeight() { return height; }
@@ -14,10 +14,10 @@ public class Image {
     /**
      * Constructeur : initialise une image vide.
      */
-    public Image(int width, int hauteur) {
+    public Image(int width, int height) {
         this.width = width;
-        this.height = hauteur;
-        pixels = new int[hauteur][width][3]; //3 = RGB
+        this.height = height;
+        pixels = new int[width][height][3];
     }
 
     /**
@@ -25,9 +25,9 @@ public class Image {
      */
     public void setPixel(int x, int y, int r, int g, int b) {
         if (x >= 0 && x < width && y >= 0 && y < height) {
-            pixels[y][x][0] = r;
-            pixels[y][x][1] = g;
-            pixels[y][x][2] = b;
+            pixels[x][y][0] = r;
+            pixels[x][y][1] = g;
+            pixels[x][y][2] = b;
         }
     }
 
@@ -35,38 +35,43 @@ public class Image {
      * Sauvegarde l'image au format texte PPM (P3)
      */
     public void save_txt(String filename) throws IOException {
-        FileWriter writer = new FileWriter(filename);
-        writer.write("P3\n");
-        writer.write(width + " " + height + "\n");
-        writer.write("255\n");
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                writer.write(pixels[y][x][0] + " " + pixels[y][x][1] + " " + pixels[y][x][2] + " ");
+        try (FileWriter writer = new FileWriter(filename)) {
+            writer.write("P3\n");
+            writer.write("200 100\n");
+            writer.write("255\n");
+            for (int y = 0; y < 100; y++) {
+                for (int x = 0; x < 200; x++) {
+                    writer.write(pixels[x][y][0] + " " + 
+                                 pixels[x][y][1] + " " + 
+                                 pixels[x][y][2] + " ");
+                }
+                writer.write("\n");
             }
-            writer.write("\n");
+        writer.close();
+        }
+    }
+
+    /**
+     * Sauvegarde l'image au format texte / binaire
+     */
+    public void save_bin(String filename) throws IOException {
+        FileOutputStream writer = new FileOutputStream(filename);
+
+        // écriture de l'entête du fichier binarie PPM
+        String entete = "P6\n" + width + " " + height + "\n255\n";
+        writer.write(entete.getBytes());
+
+        byte[] pixel = new byte[3];
+
+        for (int y = 0; y < 100; y++) {
+            for (int x = 0; x < 200; x++) {
+                pixel[0] = (byte) pixels[x][y][0];
+                pixel[1] = (byte) pixels[x][y][1];
+                pixel[2] = (byte) pixels[x][y][2];
+                writer.write(pixel);
+            }
         }
         writer.close();
     }
-	
-	/**
-     * Sauvegarde l'image au format binaire PPM (P6)
-     */
-    public void write_bin(String filename) throws IOException {
-        FileOutputStream out = new FileOutputStream(filename);
 
-        // En-tête : en texte (ASCII), donc on l'écrit avec getBytes()
-        String header = "P6\n" + width + " " + height + "\n255\n";
-        out.write(header.getBytes());
-
-        // Corps : chaque valeur R, G, B est un octet brut (0-255)
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                out.write(pixels[y][x][0]); // R
-                out.write(pixels[y][x][1]); // G
-                out.write(pixels[y][x][2]); // B
-            }
-        }
-
-        out.close();
-    }
 }

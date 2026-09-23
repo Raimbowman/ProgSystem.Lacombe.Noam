dca272671358b0c964c69d07b42da1d4fba0e32b
Noaml <noaml@gmail.com>
2026-09-23 16:03:18 +0200

    ajoute le writeShort

--- SystemeFichiersVirtuel/Utils.java
+++ SystemeFichiersVirtuel/Utils.java
@@ -19,7 +19,8 @@ public class Utils {
     }
 
     public static int writeShort(byte[] memory, int offset, short value) {
-        // TODO: Écrire les 2 octets de 'value'.
+        memory[offset]     = (byte)((value >> 8) & 0xFF);
+        memory[offset + 1] = (byte)( value & 0xFF);
         return 2;
     }
 
